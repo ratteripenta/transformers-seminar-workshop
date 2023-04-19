e370608064937a241db00d2100e831b411ce0bc8
Petteri Ranta <petteri.ranta@tietoevry.com>
2023-04-19 09:02:24 +0300

    Added stuff

--- notebooks/workshop-2/visualize.py
+++ notebooks/workshop-2/visualize.py
@@ -15,8 +15,8 @@ def visualize_self_attention(
     v: torch.Tensor = None,
 ):
     """
-    Visualize the self-attention mechanism for each token in the sequence using heatmaps with annotations. 
-    
+    Visualize the self-attention mechanism for each token in the sequence using heatmaps with annotations.
+
     To do this, provide heatmaps with annotations for input embeddings, raw and scaled attention weights, and output embeddings after applying self-attention. If query, key, and value weight vectors used in the attention mechanism are provided, visualize them as well. To do this, provide the q, k, and v arguments with the corresponding weight vectors, and the function will generate heatmaps with annotations for each of them.
 
     Args:
@@ -33,10 +33,6 @@ def visualize_self_attention(
     fig_width = max(4, x.shape[1]) * fig_scale
     token_fig_height = 0.5 * x.shape[2]
     weight_fig_height = weights.shape[2]
-    token_labels = [f"$T_{i}$" for i in range(x.shape[1])]
-    weight_labels = [f"$W^T_{i}$" for i in range(weights.shape[1])]
-    embedding_labels = [f"$E_{i}$" for i in range(x.shape[2])]
-    weight_embedding_labels = [f"$W^E_{i}$" for i in range(weights.shape[2])]
     common_args = dict(
         annot=True,
         cbar=False,
@@ -45,11 +41,11 @@ def visualize_self_attention(
     plt.figure(figsize=(fig_width, token_fig_height))
     sns.heatmap(
         x[0, ::].T,
-        xticklabels=token_labels,
-        yticklabels=embedding_labels,
+        xticklabels=[f"$T_{i}$" for i in range(x.shape[1])],
+        yticklabels=[],
         **common_args,
     )
-    plt.title("Input embedding values ($S_0$)")
+    plt.title("Input embedding vectors ($S_0$)")
     plt.show()
 
     if q is not None and k is not None and v is not None:
@@ -57,18 +53,18 @@ def visualize_self_attention(
             plt.figure(figsize=(fig_width, token_fig_height))
             sns.heatmap(
                 value[0, ::].T,
-                xticklabels=[f"${name.upper()}_{i}$" for i in range(value.shape[1])],
-                yticklabels=[f"$W^{name.upper()}_{i}$" for i in range(value.shape[2])],
+        xticklabels=[],
+                yticklabels=[f"${name.upper()}_{i}$" for i in range(value.shape[2])],
                 **common_args,
             )
-            plt.title(f"{name.upper()} weight values ($S_0$)")
+            plt.title(f"{name.upper()} vectors ($S_0$)")
             plt.show()
 
     plt.figure(figsize=(fig_width, 0.5 * weight_fig_height))
     sns.heatmap(
         raw_weights[0, ::].T,
-        xticklabels=weight_labels,
-        yticklabels=weight_embedding_labels,
+        xticklabels=[],
+        yticklabels=[],
         **common_args,
     )
     plt.title("Raw weights for tokens ($S_0$)")
@@ -77,8 +73,8 @@ def visualize_self_attention(
     plt.figure(figsize=(fig_width, 0.5 * weight_fig_height))
     sns.heatmap(
         weights[0, ::].T,
-        xticklabels=weight_labels,
-        yticklabels=weight_embedding_labels,
+        xticklabels=[],
+        yticklabels=[],
         **common_args,
     )
     plt.title("Scaled weights for tokens ($S_0$)")
@@ -87,8 +83,8 @@ def visualize_self_attention(
     plt.figure(figsize=(fig_width, token_fig_height))
     sns.heatmap(
         y[0, ::].T,
-        xticklabels=token_labels,
-        yticklabels=embedding_labels,
+        xticklabels=[f"$T_{i}$" for i in range(x.shape[1])],
+        yticklabels=[],
         **common_args,
     )
     plt.title("Self-attention output ($S_0$)")
